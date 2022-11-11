0869ca5a75e7d228a644495ab3e14adcda935df7
Octavio Lucardi Fierro <octaviolf2016@gmail.com>
2022-11-11 12:26:01 -0300

    css a medio hacer

--- php/index.php
+++ php/index.php
@@ -5,24 +5,34 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <link rel="stylesheet" href="../css/style.css">
     <title>Inicio de sesión</title>
 
 </head>
 
-<body>
-
-    <form method="post">
-        <input type="text"   name="usuario"      placeholder="usuario">
-        <input type="text"   name="contraseña"   placeholder="contraseña">
-        <input type="submit" name="mandar">
+<body>  
+<section class = "contenido-login">
+    <form class = "caja1" method="post">
+        <input class = "ingrese-dato-login" type="text"   name="usuario"      placeholder="usuario"><br>
+        <input class = "ingrese-dato-login" type="text"   name="contraseña"   placeholder="contraseña"><br>
+        <input  type="submit" name="mandar">
     </form>
 
+    <div class = "caja2">
+        <a href="registroDeUsuario.php"><button class ="boton-del-login">Crear Usuario</button></a><br>
 
+        <a href="nuevacontra.html"><button class ="boton-del-login">¿Olvidaste tu contraseña?</button></a>
+    </div>
+    
+
+<<<<<<< HEAD
+</section>
+=======
     <a href="FormReg.php"><button>Crear usuario</button></a>
+>>>>>>> 7b1635dc12fd1edde3a64999b7ce9e517acdc13f
 
-    <a href="nuevacontra.html"><button>¿Olvidaste tu contraseña?</button></a>
 
+    
     <?php
     include("login.php");
     ?>
